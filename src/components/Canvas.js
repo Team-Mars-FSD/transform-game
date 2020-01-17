69a2ec7f0b265d539db7d0486b73449f69b6fd39
unknown <tgarmita@gmail.com>
2020-01-16 17:21:19 -0700

    add react spring animation component

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import TriangleShape from './TriangleShape.js';
 import { Triangle } from './triangle.js';
-import { evaluateMatch } from './evaluate.js'
+import { evaluateMatch } from './evaluate.js';
+import Animation from './Animation.js';
 
 class Canvas extends Component {
   constructor(props) {
@@ -9,16 +10,11 @@ class Canvas extends Component {
     this.state = {
       translateX: "",
       translateY: "",
+      animate: false,
       moveCounter: 0
     }
     this.goal = new Triangle(7, -6, 5, -8, 7, -8);
     this.player = new Triangle(-4, 3, -4, 1, -2, 1);
-
-    this.playerStyle = {
-      fill: "rgb(204, 130, 238)",
-      stroke: "rgb(104, 56, 126)",
-      strokeWidth: 2
-    }
   }
 
   handleOnChange = event => {
@@ -27,14 +23,20 @@ class Canvas extends Component {
     });
   }
 
-  handleTranslate = () => {
-    this.player.translate(Number(this.state.translateX), Number(this.state.translateY));
-    this.setState(state => ({
-      translateX: "",
-      translateY: "",
-      moveCounter: state.moveCounter + 1
+  handleTranslate = async () => {
+    await this.setState(state => ({
+      animate: !state.animate
     }));
-  }
+    setTimeout(() => {
+      this.player.translate(Number(this.state.translateX), Number(this.state.translateY));
+      this.setState(state => ({
+        animate: !state.animate,
+        moveCounter: state.moveCounter + 1
+        //translateX: "", enable later
+        //translateY: "",
+      }));
+    }, 1000);
+  };
 
   handleRotate = (deg) => {
     this.player.rotate(0, 0, deg);
@@ -108,8 +110,18 @@ class Canvas extends Component {
           <text x="505" y="15">10</text>
           <text x="980" y="515">10</text>
 
-          <TriangleShape a={this.goal.a} b={this.goal.b} c={this.goal.c} />
-          <TriangleShape a={this.player.a} b={this.player.b} c={this.player.c} style={this.playerStyle} />
+          <TriangleShape triangleClassName={"goal"} a={this.goal.a} b={this.goal.b} c={this.goal.c} />
+          <TriangleShape triangleClassName={"player"} a={this.player.a} b={this.player.b} c={this.player.c} animate={this.state.animate} />
+
+          <Animation
+            triangleClassName={"player"}
+            a={this.player.a}
+            b={this.player.b}
+            c={this.player.c}
+            animate={this.state.animate}
+            translateX={Number(this.state.translateX)}
+            translateY={Number(this.state.translateY)}
+          />
 
           <text className={win === "WIN!" ? "win" : null} x="300" y="500">{win}</text>
 
--- src/components/TriangleShape.js
+++ src/components/TriangleShape.js
@@ -8,16 +8,19 @@ const TriangleShape = (props) => {
   const ccx = (10 + props.c.x) * 50
   const ccy = (10 - props.c.y) * 50
 
+
   return (
     <>
-        <polygon className="triangle"
+      {!props.animate ? (
+        <polygon className={props.triangleClassName}
           points={`${cax} ${cay}, ${cbx} ${cby}, ${ccx} ${ccy}`}
-          style={props.style}>
+        >
         </polygon>
+      ) : null}
 
-        {/* point labels */}
-        <text fontWeight="bold" x={cax} y={cay - (Math.sign(props.a.y) * 10)}>{`(${props.a.x}, ${props.a.y})`}</text>
-        {/* <text font-weight="bold" x={cbx} y={cby - (Math.sign(props.b.y) * 10)}>{`(${props.b.x}, ${props.b.y})`}</text>
+      {/* point labels */}
+      {/* <text fontWeight="bold" x={cax} y={cay - (Math.sign(props.a.y) * 10)}>{`(${props.a.x}, ${props.a.y})`}</text> */}
+      {/* <text font-weight="bold" x={cbx} y={cby - (Math.sign(props.b.y) * 10)}>{`(${props.b.x}, ${props.b.y})`}</text>
         <text font-weight="bold" x={ccx} y={ccy - (Math.sign(props.c.y) * 10)}>{`(${props.c.x}, ${props.c.y})`}</text> */}
     </>
   );
